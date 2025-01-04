4a6eeb0a7927d06c2ae60385bf7b0a9135ae41f1
wtechtec <504105925@qq.com>
2025-01-04 09:59:00 +0800

    更新spm

--- Package.swift
+++ Package.swift
@@ -4,18 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "sherpa-onnx-framework ",
+    name: "sherpa-onnx-framework",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "sherpa-onnx-framework ",
-            targets: ["sherpa-onnx-framework "]),
+            name: "sherpa-onnx-framework",
+            targets: ["sherpa-onnx-framework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "sherpa-onnx-framework "),
+            name: "sherpa-onnx-framework"),
+        .binaryTarget(
+                    name: "sherpa-onnx",
+                    headers: .public("macos-arm64_x86_64/Headers")  // 配置头文件目录
+                )
 
     ]
 )
